32b6ce4766dda355d4d07ac43de624a055c0a39e
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-06-08 20:57:48 -0300

    Update OutdatedSubState.hx

--- source/OutdatedSubState.hx
+++ source/OutdatedSubState.hx
@@ -71,7 +71,9 @@ class OutdatedSubState extends MusicBeatState
 		txt.borderStyle = FlxTextBorderStyle.OUTLINE;
 		txt.screenCenter();
 		add(txt);
-		
+		#if android
+                addVirtualPad(NONE, A);
+                #end
 		FlxTween.color(bg, 2, bg.color, FlxColor.fromString(bgColors[colorRotation]));
 		FlxTween.angle(kadeLogo, kadeLogo.angle, -10, 2, {ease: FlxEase.quartInOut});
 		
